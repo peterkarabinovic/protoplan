27570e46d48efe30d0d918038c02fe1bb6317797
Peter <petr.karabinovic@live.com>
2017-07-20 16:49:09 +0300

    fix urls

--- web3/src/middleware/requests.js
+++ web3/src/middleware/requests.js
@@ -10,22 +10,22 @@ export default function RequestsMiddleware(store){
             switch(action.type)
             {
                 case a.INIT:
-                    d3.json('/pavilions/')
+                    d3.json('pavilions/')
                       .get(function(pavilions){
                             store(a.PAVILIONS_LOADED, pavilions);
                       }); 
 
-                    d3.json('/bases/')
+                    d3.json('bases/')
                       .get(function(bases){
                             store(a.BASES_LOADED, bases);
                       });
 
-                    d3.json('/overlays/')
+                    d3.json('overlays/')
                       .get(function(bases){
                             store(a.OVERLAYS_LOADED, bases);
                       });
 
-                    d3.json('/stands/')
+                    d3.json('stands/')
                       .get(function(stands){
                             store(a.STANDS_LOADED, stands);
                       });
@@ -34,7 +34,7 @@ export default function RequestsMiddleware(store){
 
                 case a.PAVILION_ADD:
                     var pavi = action.payload;
-                    d3.request('/pavilions/0')
+                    d3.request('pavilions/0')
                            .mimeType("application/json")
                            .on("error", function(error) { store(a.ERROR_SET, error); })
                            .on("load", function(xhr) { 
@@ -47,7 +47,7 @@ export default function RequestsMiddleware(store){
 
                 case a.PAVILION_DELETE:
                     var id = action.payload.id;
-                    d3.request('/pavilions/'+id+'|delete')
+                    d3.request('pavilions/'+id+'|delete')
                       .post(function(er, xhr){
                             if(er) store(a.ERROR_SET, er);
                             else store(a.PAVILION_DELETED, id)
@@ -58,7 +58,7 @@ export default function RequestsMiddleware(store){
                     var base_layer = _.clone(action.payload.base);
                     delete base_layer['distance'];
                     delete base_layer['raw_svg'];
-                    d3.request('/pavilions/'+base_layer.id+'/base/')
+                    d3.request('pavilions/'+base_layer.id+'/base/')
                         .mimeType("application/json")
                         .send('POST', JSON.stringify(base_layer), function(er, xhr){
                             if(er) store(a.ERROR_SET, er.target.responseText)
@@ -72,7 +72,7 @@ export default function RequestsMiddleware(store){
 
                 case a.OVERLAY_SAVE:
                     var overlay = action.payload;
-                    d3.request('/pavilions/'+overlay.id+'/overlay/')
+                    d3.request('pavilions/'+overlay.id+'/overlay/')
                         .mimeType("application/json")
                         .send('POST', JSON.stringify(overlay), function(er, xhr){
                             if(er) store(a.ERROR_SET, er.target.responseText || 'Connection error')
@@ -87,7 +87,7 @@ export default function RequestsMiddleware(store){
                 case a.STAND_ADD:
                     var stand = action.payload;
                     var stands_id = selectedStandsId(store);
-                    d3.request('/stands/'+stands_id)
+                    d3.request('stands/'+stands_id)
                       .mimeType("application/json")
                       .send('POST', JSON.stringify(stand), function(er, xhr){
                           if(er) store(a.ERROR_SET, er.target.responseText || 'Connection error')
@@ -107,7 +107,7 @@ export default function RequestsMiddleware(store){
                     var stand = action.payload.stand;
                     stand = Immutable.set(stand, 'type', type);
                     var stands_id = selectedStandsId(store);
-                    d3.request('/stands/'+stands_id)
+                    d3.request('stands/'+stands_id)
                       .mimeType("application/json")
                       .send('POST', JSON.stringify(stand), function(er, xhr){
                           if(er) store(a.ERROR_SET, er.target.responseText || 'Connection error')
@@ -127,7 +127,7 @@ export default function RequestsMiddleware(store){
                     var points = action.payload.points;
                     stand = Immutable.set(stand, 'points', points);
                     var stands_id = selectedStandsId(store);
-                    d3.request('/stands/'+stands_id)
+                    d3.request('stands/'+stands_id)
                       .mimeType("application/json")
                       .send('POST', JSON.stringify(stand), function(er, xhr){
                           if(er) store(a.ERROR_SET, er.target.responseText || 'Connection error')
@@ -145,7 +145,7 @@ export default function RequestsMiddleware(store){
                 case a.STAND_DELETE:
                     var stand = action.payload;
                     var stands_id = selectedStandsId(store);
-                    d3.request('/stands/'+stands_id+'/'+stand.id+'|delete')
+                    d3.request('stands/'+stands_id+'/'+stand.id+'|delete')
                       .mimeType("application/json")
                       .get(function(er, xhr){
                             if(er) store(a.ERROR_SET, er);
